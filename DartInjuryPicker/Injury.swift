85f04b6354320bb1e5bcd1d63d766dafdd545b63
CorvusCorvid <105397358+CorvusCorvid@users.noreply.github.com>
2026-04-15 09:52:52 -0500

    Injuries

--- DartInjuryPicker/Injury.swift
+++ DartInjuryPicker/Injury.swift
@@ -7,12 +7,33 @@
 
 import SwiftUI
 
-struct Injury: View {
-    var body: some View {
-        Text("help")
+enum Injury: String, Identifiable, CaseIterable {
+    case skullFracture, punctureWound, blindness, laceration, brainTrauma, death
+    
+    var id: String {self.rawValue}
+    
+    var message: String {
+        
+        switch self {
+        case .skullFracture: return "Skull Fracture"
+        case .punctureWound: return "Puncture Wound"
+        case .blindness: return "Blindness"
+        case .laceration: return "Laceration"
+        case .brainTrauma: return "Brain Trauma"
+        case .death: return "Death"
+        }
     }
-}
-
-#Preview {
-    Injury()
+    
+    var symbol: String{
+        switch self{
+        case .skullFracture: return "🤕"
+        case .punctureWound: return "🩹"
+        case .blindness: return "👁️"
+        case .laceration: return "🩸"
+        case .brainTrauma: return "🧠"
+        case .death: return "💀"
+        }
+    }
+    
+    
 }
