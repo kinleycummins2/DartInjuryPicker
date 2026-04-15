b8a82eca8ccab3dd1a5837cd6044feab9506737b
kc0928941 <kc0928941@otc.edu>
2026-04-15 09:13:24 -0500

    Initial Commit

---
+++ DartInjuryPicker/DartInjuryPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DartInjuryPickerApp.swift
+//  DartInjuryPicker
+//
+//  Created by CUMMINS, KINLEY on 4/15/26.
+//
+
+import SwiftUI
+
+@main
+struct DartInjuryPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
